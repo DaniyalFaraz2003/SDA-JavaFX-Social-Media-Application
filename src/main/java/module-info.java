40a0ef8a76fbc1882ca5e998c81718643bc46791
Daniyal Faraz <daniyalfaraz2003@gmail.com>
2024-10-14 17:08:05 +0500

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.sdaprojectsocialmediaapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.sdaprojectsocialmediaapp to javafx.fxml;
+    exports com.example.sdaprojectsocialmediaapp;
+}
