470df623d0c27add3ab82a5af113c4f92a89de2d
HammadAli132 <108983041+HammadAli132@users.noreply.github.com>
2024-11-11 20:47:15 +0500

    Homepage Sidebar updated

--- src/main/java/com/example/sdaprojectsocialmediaapp/MainApp.java
+++ src/main/java/com/example/sdaprojectsocialmediaapp/MainApp.java
@@ -1,9 +1,10 @@
 package com.example.sdaprojectsocialmediaapp;
 import com.example.sdaprojectsocialmediaapp.models.HomePageApp;
+import com.example.sdaprojectsocialmediaapp.models.RegisterApp;
 import javafx.application.Application;
 
 public class MainApp {
     public static void main(String[] args) {
-        Application.launch(HomePageApp.class, args);
+        Application.launch(RegisterApp.class, args);
     }
 }
