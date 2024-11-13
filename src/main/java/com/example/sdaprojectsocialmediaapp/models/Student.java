e48df732a41ea84ddfa5c6d39b5f8712e27e2c21
Daniyal Faraz <daniyalfaraz2003@gmail.com>
2024-11-13 10:40:23 +0500

    student fixes

--- src/main/java/com/example/sdaprojectsocialmediaapp/models/Student.java
+++ src/main/java/com/example/sdaprojectsocialmediaapp/models/Student.java
@@ -7,19 +7,15 @@ public class Student {
     private String name;
     private String userName;
     private String password;
+    private String email;
+    private String phone;
 
-    public Student(String name, String userName, String password) {
+    public Student(String name, String userName, String password, String email, String phone) {
         this.name = name;
         this.userName = userName;
         this.password = password;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
+        this.email = email;
+        this.phone = phone;
     }
 
     public String getName() {
@@ -28,6 +24,14 @@ public class Student {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getUserName() {
@@ -46,14 +50,34 @@ public class Student {
         this.password = password;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+
     @Override
     public String toString() {
 
         return "Student{" +
                 "id=" + id +
-                ", firstName='" + name + '\'' +
+                ", name='" + name + '\'' +
                 ", userName='" + userName + '\'' +
                 ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
                 '}';
     }
 
