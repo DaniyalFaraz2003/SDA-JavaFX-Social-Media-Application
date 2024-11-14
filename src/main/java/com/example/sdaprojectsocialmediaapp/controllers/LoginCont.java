706f734bff1bd5f8dc74ce7daa8c70d949191d3e
unknown <amnasiddiqui308@gmail.com>
2024-11-14 19:47:40 +0500

    Login Controller Implemented

--- src/main/java/com/example/sdaprojectsocialmediaapp/controllers/LoginCont.java
+++ src/main/java/com/example/sdaprojectsocialmediaapp/controllers/LoginCont.java
@@ -1,10 +1,13 @@
 package com.example.sdaprojectsocialmediaapp.controllers;
 
 import com.example.sdaprojectsocialmediaapp.Router;
+import com.example.sdaprojectsocialmediaapp.repository.StudentRepository;
+import com.example.sdaprojectsocialmediaapp.utils.Validate;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
@@ -14,13 +17,17 @@ import java.io.IOException;
 import java.util.Objects;
 
 
-public class LoginCont {
+public class  LoginCont {
 
+    StudentRepository studentRepository = new StudentRepository();
     @FXML
     private PasswordField i_password;
 
     @FXML
     private TextField i_username;
+
+    @FXML
+    private Label err;
 
     @FXML
     private Stage stage;
@@ -37,10 +44,20 @@ public class LoginCont {
 
     @FXML
     void login(MouseEvent event) throws IOException {
-        String username = i_username.getText();
-        String password = i_password.getText();
-        // Further session begins
-        Router.navigateTo("Homepage");
+        String username = i_username.getText().trim();
+        String password = i_password.getText().trim();
+        if(Validate.isValidUsername(username)) {
+            if(studentRepository.checkStudentExists(username, password)) {
+                // Further session begins
+                Router.navigateTo("Homepage");
+            }
+            else{
+                err.setText("Invalid Username or Password");
+            }
+        }
+        else{
+            err.setText("Invalid Username or Password");
+        }
     }
 
 }
