88a427e66f8e6546a94cc900bbe3041609b79519
Daniyal Faraz <daniyalfaraz2003@gmail.com>
2024-11-19 20:37:25 +0500

    test scripts

---
+++ src/main/resources/database/TestScripts.sql
@@ -0,0 +1,11 @@
+UPDATE simple_post
+SET picture_url = '/post_images/1.jpeg'
+WHERE post_id = 1;
+
+UPDATE simple_post
+SET picture_url = '/post_images/2.jpeg'
+WHERE post_id = 2;
+
+UPDATE simple_post
+SET picture_url = '/post_images/3.jpeg'
+WHERE post_id = 3;
