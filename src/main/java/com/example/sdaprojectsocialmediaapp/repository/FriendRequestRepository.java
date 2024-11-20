9342af0bdb56a4aac38030ca6bf9c3ed55b967e5
unknown <amnasiddiqui308@gmail.com>
2024-11-20 13:36:55 +0500

    FriendRequestRepository and DB updated.

--- src/main/java/com/example/sdaprojectsocialmediaapp/repository/FriendRequestRepository.java
+++ src/main/java/com/example/sdaprojectsocialmediaapp/repository/FriendRequestRepository.java
@@ -1,8 +1,11 @@
 package com.example.sdaprojectsocialmediaapp.repository;
 
+import com.example.sdaprojectsocialmediaapp.models.FriendRequestComp;
+import com.example.sdaprojectsocialmediaapp.models.posts.Question;
 import com.example.sdaprojectsocialmediaapp.utils.DatabaseConnector;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class FriendRequestRepository {
     private final DatabaseConnector dbConnector;
@@ -11,7 +14,36 @@ public class FriendRequestRepository {
         dbConnector = new DatabaseConnector();
     }
 
-    public getAllFriends() {
+    public ArrayList<FriendRequestComp> getAllFriendsRequests(int studentID) {
+        ArrayList<FriendRequestComp> friendRequests = new ArrayList<>();
+        String sql = "SELECT * FROM Friend WHERE student_id_to = ? and status = ?";
+        boolean status = false;
 
+        try(Connection conn = dbConnector.getConnection()) {
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+
+            pstmt.setInt(1, studentID);
+            pstmt.setBoolean(2, status);
+            ResultSet rs = pstmt.executeQuery();
+            while(rs.next()) {
+                int id = rs.getInt("id");
+                int fromID = rs.getInt("student_id_from");
+                int toID = rs.getInt("student_id_to");
+                Timestamp timeStamp = rs.getTimestamp("time_stamp");
+
+                FriendRequestComp comp = new FriendRequestComp(id, fromID, toID, timeStamp, status);
+                friendRequests.add(comp);
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error finding friends" + e.getMessage());
+            e.printStackTrace();
+        }
+
+        if(!friendRequests.isEmpty()) {
+            return friendRequests;
+        }
+
+        return null;
     }
 }
--- src/main/resources/database/TestScripts.sql
+++ src/main/resources/database/TestScripts.sql
@@ -18,37 +18,51 @@ INSERT INTO Post (student_id, title, description, time_stamp) VALUES
                                                                   (3, 'SQL connection failed', 'My sql connection keeps failing. What can I do to prevent this?', NOW()
                                                                   );
 
+INSERT into Student (username, password, name, email, phone_number) VALUES
+                                                                        ('i221188', 'pass11', 'Ibrahim Ahmed', 'i221188@nu.edu.pk', '03377888999'),
+                                                                        ('i221988', 'pass11', 'Abdullah Ahmed', 'i221988@nu.edu.pk', '03377777999');
+
 INSERT INTO Activity_Post (post_id) VALUES
-                                        (16),
-                                        (17);
+                                        (11),
+                                        (12);
 
 INSERT INTO Simple_Post (post_id, picture_url, likes) VALUES
-    (18, null, 15);
+    (13, null, 15);
 
 INSERT INTO Question (post_id, votes) VALUES
-                                          (19, 12),
-                                          (20, 13);
+                                          (14, 12),
+                                          (15, 13);
+
 
 INSERT INTO Reply (post_id, text) VALUES
-                                      (16, "yes"),
-                                      (16, "no"),
-                                      (16, "plan it later"),
-                                      (17, "change timings"),
-                                      (17, "no"),
-                                      (17, "great plan");
+                                      (11, "yes"),
+                                      (11, "no"),
+                                      (11, "plan it later"),
+                                      (12, "change timings"),
+                                      (12, "no"),
+                                      (12, "great plan");
+
 
 INSERT INTO Student_ActivityReply (reply_id, post_id, student_id, time_stamp) VALUES
-                                                                                  (10, 16, 1, NOW()),
-                                                                                  (12, 16, 2, NOW()),
-                                                                                  (13, 17, 9, NOW()),
-                                                                                  (14, 17, 4, NOW());
+                                                                                  (10, 11, 1, NOW()),
+                                                                                  (12, 11, 2, NOW()),
+                                                                                  (13, 12, 9, NOW()),
+                                                                                  (14, 12, 4, NOW());
 
 INSERT INTO Comment (post_id, student_id, text, likes, time_stamp) VALUES
-                                                                       (18, 5, 'Amazing', 12, NOW()),
-                                                                       (18, 3, 'That is great.', 2, NOW());
+                                                                       (13, 5, 'Amazing', 12, NOW()),
+                                                                       (13, 3, 'That is great.', 2, NOW());
 
 INSERT INTO Answer (post_id, student_id, marked_correct, votes, text, time_stamp) VALUES
-                                                                                      (19, 8, TRUE, 6, 'The best way to prevent SQL injection is to use prepared statements with parameterized queries. This ensures that user input is treated as data and not part of the query structure.', NOW()),
-                                                                                      (19, 4, FALSE, 3, 'If you must use dynamic queries (which should be avoided where possible), ensure that user input is properly escaped. However, escaping is not as secure as prepared statements.', NOW()),
-                                                                                      (20, 2, TRUE, 3, 'Ensure that the MySQL server is running. If it is down, you will obviously be unable to connect.', NOW()),
-                                                                                      (20, 10, FALSE, 3, 'Check that the username, password, and database name you are using to connect to MySQL are correct.', NOW());
+                                                                                      (14, 8, TRUE, 6, 'The best way to prevent SQL injection is to use prepared statements with parameterized queries. This ensures that user input is treated as data and not part of the query structure.', NOW()),
+                                                                                      (14, 4, FALSE, 3, 'If you must use dynamic queries (which should be avoided where possible), ensure that user input is properly escaped. However, escaping is not as secure as prepared statements.', NOW()),
+                                                                                      (15, 2, TRUE, 3, 'Ensure that the MySQL server is running. If it is down, you will obviously be unable to connect.', NOW()),
+                                                                                      (15, 10, FALSE, 3, 'Check that the username, password, and database name you are using to connect to MySQL are correct.', NOW());
+
+INSERT INTO Friend (student_id_from, student_id_to, status, time_stamp) VALUES
+                                                                            (11, 12, false, NOW()),
+                                                                            (11, 1, false, NOW()),
+                                                                            (11, 3, false, NOW()),
+                                                                            (12, 4, false, NOW()),
+                                                                            (4, 11, false, NOW()),
+                                                                            (12, 3, false, NOW());
