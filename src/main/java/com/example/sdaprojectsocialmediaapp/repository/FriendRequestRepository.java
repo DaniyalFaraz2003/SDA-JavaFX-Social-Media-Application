e82654d63bcadeb3381cce19cf4bdefa8f8ff4f9
Daniyal Faraz <daniyalfaraz2003@gmail.com>
2024-11-20 13:06:22 +0500

    temp

--- src/main/java/com/example/sdaprojectsocialmediaapp/repository/FriendRequestRepository.java
+++ src/main/java/com/example/sdaprojectsocialmediaapp/repository/FriendRequestRepository.java
@@ -1,6 +1,10 @@
 package com.example.sdaprojectsocialmediaapp.repository;
 
 import com.example.sdaprojectsocialmediaapp.models.FriendRequestComp;
+import com.example.sdaprojectsocialmediaapp.models.engagements.Reply;
+import com.example.sdaprojectsocialmediaapp.models.engagements.StudentActivityReply;
+import com.example.sdaprojectsocialmediaapp.models.posts.ActivityPost;
+import com.example.sdaprojectsocialmediaapp.models.posts.Question;
 import com.example.sdaprojectsocialmediaapp.utils.DatabaseConnector;
 
 import java.sql.*;
@@ -22,10 +26,27 @@ public class FriendRequestRepository {
         ArrayList<FriendRequestComp> friends = new ArrayList<>();
         boolean status = true;
 
-        try (Connection conn = dbConnector.getConnection()) {
-            PreparedStatement pstmt;
-        }
+        try(Connection conn = dbConnector.getConnection()) {
+            PreparedStatement pstmt = conn.prepareStatement(sql);
 
+            pstmt.setInt(1, studentId);
+            pstmt.setInt(2, studentId);
+            pstmt.setBoolean(3, status);
+            ResultSet rs1 = pstmt.executeQuery();
+
+            while (rs1.next()) {
+                int id = rs1.getInt("ID");
+                int id_from = rs1.getInt("student_id_from");
+                int id_to = rs1.getInt("student_id_to");
+                boolean st = rs1.getBoolean("status");
+
+                FriendRequestComp fr = new FriendRequestComp(id, id_from, id_to)
+
+            }
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
-    }
+
 }
