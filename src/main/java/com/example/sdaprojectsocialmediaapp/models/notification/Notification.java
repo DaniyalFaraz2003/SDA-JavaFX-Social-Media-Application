eba996fa070d883c395926cf5f65ef6e148fe578
Daniyal Faraz <daniyalfaraz2003@gmail.com>
2024-11-23 19:39:38 +0500

    notification class

--- src/main/java/com/example/sdaprojectsocialmediaapp/models/notification/Notification.java
+++ src/main/java/com/example/sdaprojectsocialmediaapp/models/notification/Notification.java
@@ -1,5 +1,59 @@
 package com.example.sdaprojectsocialmediaapp.models.notification;
 
-public class Notification {
+import java.sql.Timestamp;
 
+public class Notification {
+    private int id;
+    private int senderId;
+    private int recieverId;
+    private String message;
+    private Timestamp timestamp;
+
+    public Notification(int id, int senderId, int recieverId, String message, Timestamp timestamp) {
+        this.id = id;
+        this.senderId = senderId;
+        this.recieverId = recieverId;
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(int senderId) {
+        this.senderId = senderId;
+    }
+
+    public int getRecieverId() {
+        return recieverId;
+    }
+
+    public void setRecieverId(int recieverId) {
+        this.recieverId = recieverId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
 }
